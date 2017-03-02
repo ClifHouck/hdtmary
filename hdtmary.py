da158725158691b6258306e1b6152b8fa8b7dd52
Clif Houck <me@clifhouck.com>
2017-03-02 13:21:15 -0600

    Make it videos instead of images

--- hdtmary.py
+++ hdtmary.py
@@ -5,7 +5,7 @@ import flask
 
 app = flask.Flask(__name__)
 
-IMAGE_DIR = "/home/clif/git_repos/hdtmary/static/images/"
+IMAGE_DIR = "/var/www/hdtmary/static/images/"
 IMAGE_LIST = []
 
 
@@ -17,10 +17,20 @@ def scan_images_dir():
 app.before_first_request(scan_images_dir)
 
 
+VIDEO_LIST = ['ZbM6WbUw7Bs',
+              '3T4TozPnUR4',
+              'K8ZDlDF2s_4',
+              'T2i_R7zUPVw']
+
 @app.route("/")
 def homepage():
-    image = random.choice(IMAGE_LIST)
-    return flask.render_template('index.html', img_src=image)
+    image = ''
+    video = random.choice(VIDEO_LIST)
+    response = flask.make_response(flask.render_template('index.html', img_src=image, video=video))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    return response
+
 
 
 if __name__ == "__main__":
